e844400138e6c586ec16c380e4ca13c9a412c1fe
himanshi <himanshigaire662@gmail.com>
2024-01-29 17:33:58 +0545

    more changes

--- js/main.js
+++ js/main.js
@@ -23,25 +23,24 @@
         } else {
             if ($(this).scrollTop() > 55) {
                 $('.fixed-top').addClass('shadow').css('top', -55),
-                $('.fixed-top').addClass('whitenav').css('background','#ffffff');
+                    $('.fixed-top').addClass('whitenav').css('background', '#ffffff');
             } else {
                 $('.fixed-top').removeClass('shadow').css('top', 0),
-                $('.fixed-top').removeClass('whitenav').css('background','transparent');
+                    $('.fixed-top').removeClass('whitenav').css('background', 'transparent');
             }
-        } 
+        }
     });
-    
-    
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 0, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 0, 'easeInOutExpo');
         return false;
     });
 
@@ -54,27 +53,27 @@
         dots: true,
         loop: true,
         margin: 25,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:1
+            576: {
+                items: 1
             },
-            768:{
-                items:1
+            768: {
+                items: 1
             },
-            992:{
-                items:2
+            992: {
+                items: 2
             },
-            1200:{
-                items:2
+            1200: {
+                items: 2
             }
         }
     });
@@ -88,27 +87,27 @@
         dots: true,
         loop: true,
         margin: 25,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:1
+            576: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             },
-            1200:{
-                items:4
+            1200: {
+                items: 4
             }
         }
     });
