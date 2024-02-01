be4ced13bda61f8b83377e0d31f89ad0b4a5d9ce
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-02-01 17:43:35 +0545

    navbar.php

--- shop-detail.php
+++ shop-detail.php
@@ -47,55 +47,9 @@ $pdetails = mysqli_query($conn, "SELECT * FROM products where product_id = '$pro
 
 
     <!-- Navbar start -->
-    <div class="fixed-top bg-white">
-        <div class="topbar bg-primary">
-            <div class="d-flex justify-content-between">
-                <div class="top-info ps-2">
-                    <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#"
-                            class="text-white">Kathmandu, Nepal</a></small>
-                    <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#"
-                            class="text-white">gaunkoachar@gmail.com</a></small>
-                </div>
-                <div class="top-link pe-2">
-                    <small class="me-3"><i class="fas fa-phone-alt me-2 text-secondary"></i> <a href="#"
-                            class="text-white">+977 9843011770</a></small>
-                </div>
-            </div>
-        </div>
-        <div class="container px-0 bg-white ">
-            <nav class="navbar navbar-light navbar-expand-xl">
-                <a href="index.html" class="navbar-brand">
-                    <h1 class="display-6"><img src="./img/gaunkoachar.png" class="main-logo" /></h1>
-                </a>
-                <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarCollapse">
-                    <span class="fa fa-bars text-primary"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <div class="navbar-nav mx-auto">
-                        <a href="index.html" class="nav-item nav-link">Home</a>
-                        <a href="shop.html" class="nav-item nav-link active">Shop</a>
-                        <a href="ourstory.html" class="nav-item nav-link">Our Story</a>
-                        <a href="contact.html" class="nav-item nav-link">Contact</a>
-                        <a href="cart.html" class="nav-item nav-link">Checkout</a>
-                    </div>
-                    <div class="d-flex p-3 me-0">
-                        <button class="btn-search btn btn-md-square rounded-circle me-4" data-bs-toggle="modal"
-                            data-bs-target="#searchModal"><i class="fas fa-2x fa-search text-primary"></i></button>
-                        <a href="cart.html" class="position-relative me-4 my-auto">
-                            <i class="fa fa-shopping-bag fa-2x"></i>
-                            <span
-                                class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1"
-                                style="top: -5px; left: 15px; height: 20px; min-width: 20px;">3</span>
-                        </a>
-                        <a href="login.html" class="my-auto">
-                            <i class="fas fa-user fa-2x"></i>
-                        </a>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </div>
+    <?php
+    include "navbar.php";
+    ?>
     <!-- Navbar End -->
 
 
