8d894f6777167b5c354527a5324f668505d53b76
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-02-02 14:55:02 +0545

    removed copyright from contact.php

--- contact.php
+++ contact.php
@@ -160,27 +160,6 @@ include('dbconnect.php');
     ?>
     <!-- Footer End -->
 
-    <!-- Copyright Start -->
-    <div class="container-fluid copyright bg-dark py-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    <span class="text-light"><a href="index.html"><i class="fas fa-copyright text-light me-2"></i>Gaun
-                            Ko Achar</a>, All right reserved.</span>
-                </div>
-                <div class="col-md-6 my-auto text-center text-md-end text-white">
-                    <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
-                    <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
-                    <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
-                    Designed and Maintained By <a class="border-bottom" href="https://project0rbit.com">Antarikshya</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Copyright End -->
-
-
-
     <!-- Back to Top -->
     <a href="#" class="btn btn-primary border-3 border-primary rounded-circle back-to-top"><i
             class="fa fa-arrow-up"></i></a>
