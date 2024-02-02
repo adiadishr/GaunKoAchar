b92e5d82500e952eb438e4faa4abbe10049c40c5
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-02-02 14:12:01 +0545

    navbar fix

--- shop.php
+++ shop.php
@@ -1,10 +1,9 @@
 <?php
-    session_start();
-    include "dbconnect.php";
-    include('navbar.php');
-    $products = mysqli_query($conn, "SELECT * FROM products");
+session_start();
+include "dbconnect.php";
+$products = mysqli_query($conn, "SELECT * FROM products");
+?>
 
- ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,9 +18,7 @@
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Raleway:wght@600;800&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Raleway:wght@600;800&display=swap" rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
@@ -42,11 +39,16 @@
 <body>
 
     <!-- Spinner Start -->
-    <div id="spinner"
-        class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
+    <div id="spinner" class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
         <div class="spinner-grow text-primary" role="status"></div>
     </div>
     <!-- Spinner End -->
+
+    <!-- Navbar Start -->
+    <?php
+    include('navbar.php');
+    ?>
+    <!-- Navbar End -->
 
     <!-- Modal Search Start -->
     <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -58,8 +60,7 @@
                 </div>
                 <div class="modal-body d-flex align-items-center">
                     <div class="input-group w-75 mx-auto d-flex">
-                        <input type="search" class="form-control p-3" placeholder="keywords"
-                            aria-describedby="search-icon-1">
+                        <input type="search" class="form-control p-3" placeholder="keywords" aria-describedby="search-icon-1">
                         <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
                     </div>
                 </div>
@@ -84,22 +85,19 @@
         <div class="container py-5">
             <h1 class="mb-4">Our Store</h1>
             <div class="row g-4">
-                <div class="col-lg-12" style= "display:flex; flex-direction:column;">
+                <div class="col-lg-12" style="display:flex; flex-direction:column;">
                     <div class="row g-4">
                         <div class="col-xl-5">
                             <div class="input-group w-100 mx-auto d-flex">
-                                <input type="search" class="form-control p-3" placeholder="Search Items"
-                                    aria-describedby="search-icon-1">
-                                <button id="btn-search" class="border-1 input-group-text py-3 px-4"><span
-                                        id="search-icon-1"><i class="fa fa-search"></i></span></button>
+                                <input type="search" class="form-control p-3" placeholder="Search Items" aria-describedby="search-icon-1">
+                                <button id="btn-search" class="border-1 input-group-text py-3 px-4"><span id="search-icon-1"><i class="fa fa-search"></i></span></button>
                             </div>
                         </div>
                         <div class="col-4"></div>
                         <div class="col-xl-3">
                             <div class="bg-light ps-3 py-3 rounded d-flex justify-content-between mb-4">
                                 <label for="fruits">Default Sorting:</label>
-                                <select id="fruits" name="fruitlist" class="border-0 form-select-sm bg-light me-3"
-                                    form="fruitform">
+                                <select id="fruits" name="fruitlist" class="border-0 form-select-sm bg-light me-3" form="fruitform">
                                     <option value="volvo">Nothing</option>
                                     <option value="saab">Popularity</option>
                                     <option value="opel">Organic</option>
@@ -109,51 +107,50 @@
                         </div>
                     </div>
 
-                        <div class="col-lg-9" style="align-self:center;">
-                            <div class="row g-4 justify-content-center">
-                                <div class="col-md-6 col-lg-6 col-xl-4">
+                    <div class="col-lg-9" style="align-self:center;">
+                        <div class="row g-4 justify-content-center">
+                            <div class="col-md-6 col-lg-6 col-xl-4">
                                 <?php
-                                while ($prod = mysqli_fetch_array($products)){?>
+                                while ($prod = mysqli_fetch_array($products)) { ?>
                                     <div class="rounded position-relative item">
                                         <div class="fruite-img">
-                                            <img src="uploads/<?php echo $prod['product_image'];?>" class="img-fluid w-100 rounded-top" alt="">
+                                            <img src="uploads/<?php echo $prod['product_image']; ?>" class="img-fluid w-100 rounded-top" alt="">
                                         </div>
                                         <div class="p-4 border border-secondary border-top-0 rounded-bottom">
-                                            <h4><?php echo $prod['product_name']?></h4>
-                                            <p><?php echo $prod['product_description']?></p>
+                                            <h4><?php echo $prod['product_name'] ?></h4>
+                                            <p><?php echo $prod['product_description'] ?></p>
 
                                             <div class="row g-4 mb-2">
-                                                <p class="text-dark fs-5 fw-bold">Rs.<?php echo $prod['product_price']?></p>
+                                                <p class="text-dark fs-5 fw-bold">Rs.<?php echo $prod['product_price'] ?></p>
                                             </div>
                                             <div class="d-flex justify-content-between flex-lg-wrap">
-                                            <?php if(isset($_SESSION['email'])){?>
-                    
-                                                <a href="shop-detail.php?product_id=<?php echo$prod["product_id"];?>" class="btn border-secondary rounded-circle">
-                                                <i class="fa fa-eye text-primary py-2 px-1"></i></a>  <?php }else{ ?>
+                                                <?php if (isset($_SESSION['email'])) { ?>
+
+                                                    <a href="shop-detail.php?product_id=<?php echo $prod["product_id"]; ?>" class="btn border-secondary rounded-circle">
+                                                        <i class="fa fa-eye text-primary py-2 px-1"></i></a> <?php } else { ?>
                                                     <a href='login.php' class='btn border-secondary rounded-circle'>
                                                         <i class="fa fa-eye text-primary py-2 px-1"></i></a> <?php } ?>
                                                 <a href="cart.php" name="Add_To_cart" class="btn border border-secondary rounded-pill px-3 text-primary">
                                                     <i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
 
                                             </div>
-                                            
+
                                         </div>
-                                        <?php }?>
-                                    </div>  
-                                </div>
-                               
-                               
-                                <div class="col-12">
-                                    <div class="pagination d-flex justify-content-center mt-5">
-                                        <a href="#" class="rounded">&laquo;</a>
-                                        <a href="#" class="active rounded">1</a>
-                                        <a href="#" class="rounded">2</a>
-                                        <a href="#" class="rounded">3</a>
-                                        <a href="#" class="rounded">4</a>
-                                        <a href="#" class="rounded">5</a>
-                                        <a href="#" class="rounded">6</a>
-                                        <a href="#" class="rounded">&raquo;</a>
+                                    <?php } ?>
                                     </div>
+                            </div>
+
+
+                            <div class="col-12">
+                                <div class="pagination d-flex justify-content-center mt-5">
+                                    <a href="#" class="rounded">&laquo;</a>
+                                    <a href="#" class="active rounded">1</a>
+                                    <a href="#" class="rounded">2</a>
+                                    <a href="#" class="rounded">3</a>
+                                    <a href="#" class="rounded">4</a>
+                                    <a href="#" class="rounded">5</a>
+                                    <a href="#" class="rounded">6</a>
+                                    <a href="#" class="rounded">&raquo;</a>
                                 </div>
                             </div>
                         </div>
@@ -162,10 +159,11 @@
             </div>
         </div>
     </div>
+    </div>
     <!-- Fruits Shop End-->
-<?php
+    <?php
     include "footer.php";
-?>
+    ?>
 </body>
 
 </html>
